03ed697da90b32d6fe1170d9c05b50d9292594bc
Elly Fong-Jones <elly@leptoquark.net>
2012-03-12 20:47:00 -0400

    cas: tabs -> spaces

--- cas.py
+++ cas.py
@@ -15,39 +15,43 @@ host = '127.0.0.1'
 port = 8080
 
 try:
-	os.stat(root)
+    os.stat(root)
 except OSError:
-	os.mkdir(root, 0700)
+    os.mkdir(root, 0700)
 
 def hash(r):
-	return hashlib.sha256(r).hexdigest()
+    print 'hash: %s' % r
+    return hashlib.sha256(r).hexdigest()
 
 def mac(r):
+    print 'mac: %s' % r
     if not key:
         return ''
-	return hmac.HMAC(key, msg=r, digestmod=hashlib.sha256).hexdigest()
+    v = hmac.HMAC(key, msg=r, digestmod=hashlib.sha256).hexdigest()
+    print '-> %s' % v
+    return v
 
 @bottle.get('/:path')
 def get(path):
-	print 'get %s' % path
-	return bottle.static_file(path, root=root)
+    print 'get %s' % path
+    return bottle.static_file(path, root=root)
 
 @bottle.post('/post/:fmac')
 def post(fmac):
-	d = bottle.request.files
-	if not d or 'file' not in d:
-		bottle.abort(400, 'Need a file')
-	dn = tempfile.mkdtemp(prefix='/tmp/cas-upload-')
-	with open(dn + '/file', 'w') as f:
-		r = d['file'].file.read()
-		f.write(r)
-		v = hash(r)
-		m = mac(v)
-		if key and m != fmac:
-			bottle.abort(400, 'HMAC failure')
-		with open(root + '/' + v, 'w') as f0:
-			f0.write(r)
-		return me + v + '\n'
+    d = bottle.request.files
+    if not d or 'file' not in d:
+        bottle.abort(400, 'Need a file')
+    dn = tempfile.mkdtemp(prefix='/tmp/cas-upload-')
+    with open(dn + '/file', 'w') as f:
+        r = d['file'].file.read()
+        f.write(r)
+        v = hash(r)
+        m = mac(v)
+        if key and hash(m) != hash(fmac):
+            bottle.abort(400, 'HMAC failure')
+        with open(root + '/' + v, 'w') as f0:
+            f0.write(r)
+        return me + v + '\n'
 
 bottle.debug()
 bottle.run(host=host, port=port)
